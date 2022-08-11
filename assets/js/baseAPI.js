9d8197d31be4a91d1a9b6de7cf06c7299e343550
prx <2646477320@qq.com>
2022-08-11 13:32:21 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,19 @@
 $.ajaxPrefilter(function(options) {
     // 发起ajax请求之前，主动拼接请求的url地址
     options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    // 统一为有权限的接口设置headers请求头
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token')||''
+        }
+    }
+    options.complete = function(res) {
+        // console.log(res);
+        if (res.responseJSON.status === 1&& res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href='/login.html'
+        }
+        
+    }
 })
